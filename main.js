2864479929956aba4ddc53c8f4e3bb7e326d3023
oliverlo1 <113052541+oliverlo1@users.noreply.github.com>
2022-11-07 11:22:58 -0700

    awd

---
+++ main.js
@@ -0,0 +1,38 @@
+let cnv = document.getElementById("myCanvas");
+let ctx = cnv.getContext("2d");
+cnv.width = 800;
+cnv.height = 600;
+
+requestAnimationFrame(draw);
+function draw() {
+    ctx.fillStyle = "black";
+    ctx.fillRect(0, 0, cnv.width, cnv.height);
+
+    let x = +document.getElementById("x-value").value;
+    let y = +document.getElementById("y-value").value;
+    let r = +document.getElementById("r-value").value;
+    let numRings = +document.getElementById("number-rings").value;
+    let gaga = numRings;
+    let newR = r / gaga;
+
+    if (numRings > 0) {
+        ctx.lineWidth = 3;
+        ctx.strokeStyle = "red";
+        ctx.beginPath();
+        ctx.arc(x, y, r, 0, 2 * Math.PI);
+        // ctx.arc(x, y, r, startAngle, endAngle);
+        ctx.stroke();
+
+        for (i = 0; i < numRings; i++) {
+            ctx.lineWidth = 3;
+            ctx.strokeStyle = "red";
+            ctx.beginPath();
+            ctx.arc(x, y, newR, 0, 2 * Math.PI);
+            // ctx.arc(x, y, r, startAngle, endAngle);
+            ctx.stroke();
+            gaga--;
+        }
+    }
+
+    requestAnimationFrame(draw);
+}
